060757cdf64c4e48a6f6ee967bbe52124c6746ea
DVitonyte <dovile.vitonyte@gmail.com>
2016-02-05 20:27:24 +0000

    now we can track top plates and differentiate robots

--- vision/test/color_detection.py
+++ vision/test/color_detection.py
@@ -19,19 +19,23 @@ WHITE_HIGHER = np.array([36, 255, 255])
 BLUE_LOWER = np.array([70, 50, 50])
 BLUE_HIGHER = np.array([160, 255, 255])
 
-BRIGHT_BLUE_LOWER = np.array([0, 0, 0])
-BRIGHT_BLUE_HIGHER = np.array([0, 0, 0])
+BRIGHT_BLUE_LOWER = np.array([80, 80, 80])
+BRIGHT_BLUE_HIGHER = np.array([100, 255, 255])
 
 PINK_LOWER = np.array([145, 90, 90]) 
 PINK_HIGHER = np.array([170, 255, 255])
 
 RED_LOWER = np.array([0, 130, 110]) 
 RED_HIGHER = np.array([7, 255, 255])
-
+'''
+#TODO: test these values
+RED_LOWER = np.array([160, 150, 110]) 
+RED_HIGHER = np.array([180, 255, 255])
+'''
 GREEN_LOWER = np.array([60, 110, 110])
 GREEN_HIGHER = np.array([75, 255, 255])
 
-BRIGHT_GREEN_LOWER = np.array([40, 110, 110])
+BRIGHT_GREEN_LOWER = np.array([40, 140, 180])
 BRIGHT_GREEN_HIGHER = np.array([55, 255, 255])
 
 YELLOW_LOWER = np.array([25, 100, 100])
@@ -47,20 +51,23 @@ while(1):
 
 	hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-	#mask1 = cv2.inRange(hsv, RED_LOWER1, RED_HIGHER1)
 	yellow_mask = cv2.inRange(hsv, YELLOW_LOWER, YELLOW_HIGHER)
 	pink_mask = cv2.inRange(hsv, PINK_LOWER, PINK_HIGHER)
-	#mask = cv2.bitwise_and(mask1, mask1, mask2)
-	yellow_ret,yellow_thresh = cv2.threshold(yellow_mask,127,255,cv2.THRESH_BINARY)
-	pink_ret,pink_thresh = cv2.threshold(pink_mask,127,255,cv2.THRESH_BINARY)
+	green_mask = cv2.inRange(hsv, BRIGHT_GREEN_LOWER, BRIGHT_GREEN_HIGHER)
+	bright_blue_mask = cv2.inRange(hsv, BRIGHT_BLUE_LOWER, BRIGHT_BLUE_HIGHER)
+
+	yellow_ret, yellow_thresh = cv2.threshold(yellow_mask,127,255,cv2.THRESH_BINARY)
+	pink_ret, pink_thresh = cv2.threshold(pink_mask,127,255,cv2.THRESH_BINARY)
+	green_ret, green_thresh = cv2.threshold(green_mask,127,255,cv2.THRESH_BINARY)
+	_, bright_blue_thresh = cv2.threshold(bright_blue_mask,127,255,cv2.THRESH_BINARY)
 
 	_, yellow_contours, _ = cv2.findContours(yellow_thresh, 1, 2)
 	_, pink_contours, _ = cv2.findContours(pink_thresh, 1, 2)
-	# len(contours) represents the number of objects detected
-	#print len(contours)
+	_, green_contours, _ = cv2.findContours(green_thresh, 1, 2)
+	_, bright_blue_contours, _ = cv2.findContours(bright_blue_thresh, 1, 2)
 	
 	pink_balls = []
-	yellow_balls = []
+	#green_balls = []
 
 	for i in range(0, len(pink_contours)):
 		cnt = pink_contours[i]
@@ -76,9 +83,42 @@ while(1):
 
 		center = (int(x),int(y))
 		pink_balls.append(center)
-		print pink_balls
 		radius = int(radius)
-		#cv2.circle(frame,center,radius,(0,0,255),2)
+		cv2.circle(frame,center,radius,(0,0,255),2)
+	'''	
+	for i in range(0, len(green_contours)):
+		cnt = green_contours[i]
+		M = cv2.moments(cnt)
+		if M['m00'] == 0 :
+			continue
+
+		# center coordinates:	
+		cx = int(M['m10']/M['m00'])
+		cy = int(M['m01']/M['m00'])
+
+		(x,y),radius = cv2.minEnclosingCircle(cnt)
+
+		center = (int(x),int(y))
+		green_balls.append(center)
+		#print pink_balls
+		radius = int(radius)
+		cv2.circle(frame,center,radius,(0,255,0),2)
+	'''	
+	for i in range(0, len(bright_blue_contours)):
+		cnt = bright_blue_contours[i]
+		M = cv2.moments(cnt)
+		if M['m00'] == 0 :
+			continue
+
+		# center coordinates:	
+		cx = int(M['m10']/M['m00'])
+		cy = int(M['m01']/M['m00'])
+
+		(x,y),radius = cv2.minEnclosingCircle(cnt)
+
+		center = (int(x),int(y))
+		radius = int(radius)
+		cv2.circle(frame,center,radius,(255,255,0),2)
 
 	for i in range(0, len(yellow_contours)):
 		cnt = yellow_contours[i]
@@ -91,7 +131,6 @@ while(1):
 		cy = int(M['m01']/M['m00'])
 
 		'''
-		print cx, cy
 		print "-----------------------"
 		#---------this is for detecting top plate-----------
 		x,y,w,h = cv2.boundingRect(cnt)
@@ -103,14 +142,22 @@ while(1):
 		#mask = cv2.drawContours(mask,[box],0,(0,0,255),2)
 		cv2.rectangle(frame, (x-10,y-10),(x+w+5,y+h+5),(0,255,0),2)
 		'''
+		num_of_pink = 0
 		(x,y),radius = cv2.minEnclosingCircle(cnt)
-		#for i in range(0, len(pink_balls)):
-		#	if (math.sqrt((x-pink_balls[i][0])**2)+(y-pink_balls[1])**2) < 15:
-		#		pass
-		#print(str(x) + "  :  " + str(y))
+
+		for i in range(0, len(pink_balls)):
+			if (math.sqrt(((x-pink_balls[i][0])**2)+(y-pink_balls[i][1])**2) < 15):
+				num_of_pink += 1
+
 		center = (int(x),int(y))
 		radius = int(radius)
-		#cv2.circle(frame,center,15,(0,255,0),2)	
+		if num_of_pink == 3:
+			cv2.putText(frame,'OUR',(center[0]-15, center[1]+30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 90)
+			cv2.putText(frame,'DEFENDER',(center[0]-30, center[1]+40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 90)
+		if num_of_pink == 1:
+			cv2.putText(frame,'OUR',(center[0]-15, center[1]+30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 220)
+			cv2.putText(frame,'ATTACKER',(center[0]-30, center[1]+40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, 220)
+		cv2.circle(frame,center,15,(0,255,255),2)	
 
 	
 		
