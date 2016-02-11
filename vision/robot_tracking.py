9d3f75d64d2eff9aa682274e2623d812e28c19cb
DVitonyte <dovile.vitonyte@gmail.com>
2016-02-11 15:31:08 +0000

    code version that was run for milestone 2

--- vision/robot_tracking.py
+++ vision/robot_tracking.py
@@ -1,6 +1,6 @@
 from tracker import *
 from camera import Camera
-
+import math
 
 
 c = Camera()
@@ -69,10 +69,10 @@ while True:
     if our_orientation != None:
         _, v0 = our_orientation
         x0, y0 = our_robot_center
-        vector0 = (x0+v0[0] * 10, y0+v0[1] * 10)
+        vector0 = (x0+v0[0] * 20, y0+v0[1] * 20)
         x_0, y_0 = Tracker.transformCoordstoCV(vector0)
         our_robot_center = Tracker.transformCoordstoCV(our_robot_center)
-        cv2.line(frame, (int(our_robot_center[0]), int(our_robot_center[1])), (int(x_0),int(y_0)), (0,255,122), 2)
+        cv2.line(frame, (int(our_robot_center[0]), int(our_robot_center[1])), (int(x_0),int(y_0)), (0,0,255), 2)
         cv2.circle(frame, ( int(our_robot_center[0]), int(our_robot_center[1])), 20, our_circle_color, 2)
         cv2.putText(frame,'OUR',(int(our_robot_center[0])-15, int(our_robot_center[1])+30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, our_col)
         cv2.putText(frame,our_letters,(int(our_robot_center[0])-20, int(our_robot_center[1])+40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, our_col)
@@ -80,10 +80,10 @@ while True:
     if our_mate_orientation != None:    
         _, v1 = our_mate_orientation
         x1, y1 = our_mate_center
-        vector1 = (x1+v1[0] * 10, y1+v1[1] * 10)
+        vector1 = (x1+v1[0] * 20, y1+v1[1] * 20)
         x_1, y_1 = Tracker.transformCoordstoCV(vector1)
         our_mate_center = Tracker.transformCoordstoCV(our_mate_center)
-        cv2.line(frame, (int(our_mate_center[0]), int(our_mate_center[1])), (int(x_1),int(y_1)), (0,255,122), 2)
+        cv2.line(frame, (int(our_mate_center[0]), int(our_mate_center[1])), (int(x_1),int(y_1)), (0,0,255), 2)
         cv2.circle(frame, ( int(our_mate_center[0]), int(our_mate_center[1])), 20, our_circle_color, 2)
         cv2.putText(frame,mate_letters,(int(our_mate_center[0])-15, int(our_mate_center[1])+30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, mate_col)
         cv2.putText(frame,'TEAMMATE',(int(our_mate_center[0])-30, int(our_mate_center[1])+40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, mate_col)
@@ -92,10 +92,10 @@ while True:
     if pink_opponent_orientation != None:
         _, v2 = pink_opponent_orientation
         x2, y2 = pink_opponent_center
-        vector2 = (x2+v2[0] * 10, y2+v2[1] * 10)
+        vector2 = (x2+v2[0] * 20, y2+v2[1] * 20)
         x_2, y_2 = Tracker.transformCoordstoCV(vector2)
         pink_opponent_center = Tracker.transformCoordstoCV(pink_opponent_center)
-        cv2.line(frame, (int(pink_opponent_center[0]), int(pink_opponent_center[1])), (int(x_2),int(y_2)), (0,255,122), 2)
+        cv2.line(frame, (int(pink_opponent_center[0]), int(pink_opponent_center[1])), (int(x_2),int(y_2)), (0,0,255), 2)
         cv2.circle(frame, ( int(pink_opponent_center[0]), int(pink_opponent_center[1])), 20, opponent_circle_color, 2)
         cv2.putText(frame,'PINK',(int(pink_opponent_center[0])-15, int(pink_opponent_center[1])+30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (127,0,255))
         cv2.putText(frame,'OPPONENT',(int(pink_opponent_center[0])-30, int(pink_opponent_center[1])+40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (127,0,255))
@@ -103,10 +103,10 @@ while True:
     if green_opponent_orientation != None:
         _, v3 = green_opponent_orientation
         x3, y3 = green_opponent_center
-        vector3 = (x3+v3[0] * 10, y3+v3[1] * 10)
+        vector3 = (x3+v3[0] * 20, y3+v3[1] * 20)
         x_3, y_3 = Tracker.transformCoordstoCV(vector3)
         green_opponent_center = Tracker.transformCoordstoCV(green_opponent_center)
-        cv2.line(frame, (int(green_opponent_center[0]), int(green_opponent_center[1])), (int(x_3),int(y_3)), (0,255,122), 2)
+        cv2.line(frame, (int(green_opponent_center[0]), int(green_opponent_center[1])), (int(x_3),int(y_3)), (0,0,255), 2)
         cv2.circle(frame, ( int(green_opponent_center[0]), int(green_opponent_center[1])), 20, opponent_circle_color, 2)
         cv2.putText(frame,'GREEN',(int(green_opponent_center[0])-15, int(green_opponent_center[1])+30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,255,0))
         cv2.putText(frame,'OPPONENT',(int(green_opponent_center[0])-30, int(green_opponent_center[1])+40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,255,0))
