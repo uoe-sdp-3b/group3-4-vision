3e69166ccefffef821e0ef03acdb51736163de91
Hamish Hutchings <moredhel@aoeu.me>
2016-02-29 16:22:55 +0000

    fix infinite blocking on client
    Set up logging

--- planning/world.py
+++ planning/world.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2.7
 import zmq
 from threading import Thread
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class WorldApi():
@@ -8,7 +11,9 @@ class WorldApi():
     Wrapper around world with some threading magic to make things interesting
     """
 
-    def __init__(self, target="tcp://localhost:5555"):
+    def __init__(self, target="tcp://localhost:5555", debug=False):
+        if debug:
+            log.setLevel(logging.DEBUG)
         self.world = None
         self.target = target
         self.thread_close = False
@@ -20,22 +25,30 @@ class WorldApi():
         """
         Cleans up WorldApi
         """
+        log.debug("Closing Thread")
         self.thread_close = True
 
     def client(self):
-        print("Thread ready")
+        log.info("Client Thread ready")
         ctx = zmq.Context()
         socket = ctx.socket(zmq.SUB)
         socket.setsockopt(zmq.SUBSCRIBE, '')
+        socket.setsockopt(zmq.RCVTIMEO, 100)
         socket.connect(self.target)
 
         while not self.thread_close:
-            obj = socket.recv_pyobj()
+            obj = None
+            try:
+                obj = socket.recv_pyobj()
+            except zmq.Again as e:
+                log.warn("Thread not receiving anything")
+
             if obj is not None:
                 self.thread_ready = True
                 self.world = World(obj)
 
         socket.close()
+        log.debug("Thread Closed")
 
     # TODO: look into making this more generic
     # ugly blocking hax
@@ -83,4 +96,4 @@ class World():
             self.pink_opponent = d["pink_opponent"]
             self.green_opponent = d["green_opponent"]
         except KeyError as e:
-            print("Error accessing key " + e.message)
+            log.error("Error accessing key " + e.message)
