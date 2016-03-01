f7edf189d4739b05e659b9a3769230484c9e7376
Hamish Hutchings <moredhel@aoeu.me>
2016-03-01 23:24:52 +0000

    add formatting

--- vision/tracker.py
+++ vision/tracker.py
@@ -1,7 +1,3 @@
-from camera import Camera
-from calibrate_frame import step
-from matplotlib import pyplot as plt
-from socket import gethostname
 import numpy as np
 import cv2
 import math
@@ -9,13 +5,13 @@ from tools import *
 from algebra import *
 
 adjustments = {}
-adjustments['blur'] = (11,11) # needs to be parametrized .. TODO
-color_range = get_colors() #for PITHC=0
+adjustments['blur'] = (11, 11)  # needs to be parametrized .. TODO
+color_range = get_colors()  # for PITCH=0
+
 
 class Tracker():
-
     def denoiseMask(self, mask):
-        kernel =  cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
         po = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
         po = cv2.morphologyEx(po, cv2.MORPH_OPEN, kernel)
         return po
@@ -27,11 +23,14 @@ class Tracker():
         hsv_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
 
         if color == 'red':
-            red_mask = cv2.inRange(hsv_frame, color_range['red']['min'], color_range['red']['max'])
-            maroon_mask = cv2.inRange(hsv_frame, color_range['maroon']['min'], color_range['maroon']['max'])
+            red_mask = cv2.inRange(hsv_frame, color_range['red']['min'],
+                                   color_range['red']['max'])
+            maroon_mask = cv2.inRange(hsv_frame, color_range['maroon']['min'],
+                                      color_range['maroon']['max'])
             mask = cv2.bitwise_or(red_mask, maroon_mask)
         else:
-            mask = cv2.inRange(hsv_frame, color_range[color]['min'], color_range[color]['max'])
+            mask = cv2.inRange(hsv_frame, color_range[color]['min'],
+                               color_range[color]['max'])
 
         _, threshold = cv2.threshold(self.denoiseMask(mask), 127, 255, 0)
 
@@ -39,7 +38,7 @@ class Tracker():
 
         return self.removeUselessContours(contours)
 
-    def removeUselessContours( self, contours ) :
+    def removeUselessContours(self, contours):
         if contours is None:
             return None
         real_contours = []
@@ -57,27 +56,23 @@ class Tracker():
         center, _ = cv2.minEnclosingCircle(contour)
         return center
 
-
     # Extracts the centers of a list of contours
     def getContourCenters(self, contours):
 
-        return [ self.getContourCenter(x) for x in contours ]
-
+        return [self.getContourCenter(x) for x in contours]
 
     def getKFurthestContours(self, k, p, contours):
 
-        distances = [ distance( p, self.getContourCenter(c) ) for c in contours ]
+        distances = [distance(p, self.getContourCenter(c)) for c in contours]
         distances_sorted = np.argsort(distances)
-        return [ contours[x] for x in distances_sorted[-k:] ]
-
+        return [contours[x] for x in distances_sorted[-k:]]
 
     def getKClosestContours(self, k, p, contours):
 
-        distances = [ distance( p, self.getContourCenter(c) ) for c in contours ]
+        distances = [distance(p, self.getContourCenter(c)) for c in contours]
         distances_sorted = np.argsort(distances)
 
-        return [ contours[x] for x in distances_sorted[:k]]
-
+        return [contours[x] for x in distances_sorted[:k]]
 
     # Gives us the contour with the biggest area from a list of contours
     def getBiggestContour(self, contours):
@@ -97,12 +92,9 @@ class Tracker():
         return contours[max_contour_position]
 
 
-
 class BallTracker(Tracker):
-
     def __init__(self, ball_color):
         self.color = ball_color
-
 
     # Extracts the ( center, radius ) of our ball
     def getBallCoordinates(self, frame):
@@ -118,41 +110,43 @@ class BallTracker(Tracker):
 
 
 class RobotTracker(Tracker):
-
     def __init__(self, ally_color='yellow'):
 
         self.ally_color = ally_color
         self.side_identifiers = ['yellow', 'bright_blue']
 
-
     def getAllRobots(self, frame):
 
         helper_contours = {}
         helper_contours['pink'] = self.getContours(frame, 'pink', adjustments)
-        helper_contours['green'] = self.getContours(frame, 'green', adjustments)
+        helper_contours['green'] = self.getContours(frame, 'green',
+                                                    adjustments)
 
         robots = {}
         for side_color in self.side_identifiers:
             side_contours = self.getContours(frame, side_color, adjustments)
-            side_robots = self.getRobotCoordinates(side_contours, helper_contours['pink'])
-            if( side_color == self.ally_color ):
+            side_robots = self.getRobotCoordinates(side_contours,
+                                                   helper_contours['pink'])
+            if (side_color == self.ally_color):
                 robots['ally'] = side_robots
-            else :
+            else:
                 robots['enemy'] = side_robots
 
         for side, side_robs in robots.iteritems():
             for color, robot in side_robs.iteritems():
                 center = robot['center']
-                orientation = self.getRobotOrientation(center, helper_contours, color)
+                orientation = self.getRobotOrientation(center, helper_contours,
+                                                       color)
                 robots[side][color]['orientation'] = orientation
-
 
         return robots
 
-
     def getRobotCoordinates(self, side_contours, pink_contours):
 
-        side_robots = {'green' : { "orientation": None, "center": None}, 'pink' : { "orientation": None, "center": None}}
+        side_robots = {'green': {"orientation": None,
+                                 "center": None},
+                       'pink': {"orientation": None,
+                                "center": None}}
         for contour in side_contours:
 
             contour_center = self.getContourCenter(contour)
@@ -161,9 +155,9 @@ class RobotTracker(Tracker):
             for pink_contour in pink_contours:
                 pink_contour_center = self.getContourCenter(pink_contour)
 
-                dist = distance( pink_contour_center, contour_center )
+                dist = distance(pink_contour_center, contour_center)
 
-                if dist < 20*20 :
+                if dist < 20 * 20:
                     pink_contour_count += 1
 
             if pink_contour_count == 1:
@@ -179,11 +173,9 @@ class RobotTracker(Tracker):
 
         return side_robots
 
-
     def getRobotOrientation(self, center, helper_contours, group_color):
 
         magnitude = 30.0
-
 
         # print(helper_contours)
 
@@ -193,18 +185,21 @@ class RobotTracker(Tracker):
         if group_color == 'pink':
             main_color = 'pink'
             support_color = 'green'
-        else :
+        else:
             main_color = 'green'
             support_color = 'pink'
 
         # for _, cont in helper_contours:
         #     if cont == []:
         #         return None, None
-        orientation_support = self.getKClosestContours(1, center, helper_contours[support_color])
+        orientation_support = self.getKClosestContours(
+            1, center, helper_contours[support_color])
         support_center = self.getContourCenter(orientation_support[0])
 
-        orientation_main = self.getKClosestContours(3, center, helper_contours[main_color])
-        orientation_main = self.getKFurthestContours(2, support_center, orientation_main)
+        orientation_main = self.getKClosestContours(
+            3, center, helper_contours[main_color])
+        orientation_main = self.getKFurthestContours(2, support_center,
+                                                     orientation_main)
 
         if len(orientation_main) != 2:
             return None, None
@@ -213,11 +208,12 @@ class RobotTracker(Tracker):
         # print(main_centers)
         mean_main_point = meanPoint(main_centers)
 
-        center = transformCoordstoDecartes( center )
-        mean_main_point = transformCoordstoDecartes( mean_main_point )
+        center = transformCoordstoDecartes(center)
+        mean_main_point = transformCoordstoDecartes(mean_main_point)
 
-        direction_vector = getDirectionVector(center, mean_main_point, magnitude)
-        angle_radians = np.arctan2( direction_vector[1], direction_vector[0] )
-        angle_degrees = math.degrees( angle_radians )
+        direction_vector = getDirectionVector(center, mean_main_point,
+                                              magnitude)
+        angle_radians = np.arctan2(direction_vector[1], direction_vector[0])
+        angle_degrees = math.degrees(angle_radians)
 
         return (angle_degrees, direction_vector)
