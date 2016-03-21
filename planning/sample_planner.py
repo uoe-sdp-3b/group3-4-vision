72c4cbfe7a08f997b6254730fadd68d50f5cc3a7
DVitonyte <dovile.vitonyte@gmail.com>
2016-03-21 00:24:11 +0000

    EXAMPLE OF USABLE API - PLANNER PEOPLE: USE TI

--- planning/sample_planner.py
+++ planning/sample_planner.py
@@ -8,26 +8,7 @@ class Planner:
         api = WorldApi()
 
         try:
-            """
-             def getMyPosition():
-        return self.world[('ally', 'me')]['center']
-
-    def getAllyPosition():
-        return self.world[('ally', 'friend')]['center']
-
-    def getEnemyPositions():
-        return [self.world[('enemy', 'friend')]['center'], self.world[('enemy', 'me')]['center']]
-
-    def getMyOrientation():
-        return self.world[('ally', 'me')]['orientation']
-
-    def getAllyOrientation():
-        return self.world[('ally', 'friend')]['orientation']
-
-    def getEnemyOrientation():
-        return [self.world[('enemy', 'friend')]['orientation'], self.world[('enemy', 'me')]['orientation']]
-
-            """
+            
             while True:
                 print "My position: ", api.getMyPosition()
                 print "Friend position: ", api.getAllyPosition()
